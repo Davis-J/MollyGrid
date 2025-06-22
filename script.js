91719f3976fc436653db099ca79f963784ac92d8
Davis <davisjohnmutholil@gmail.com>
2025-06-22 15:47:09 +0100

    Fixed a bug and made updates to modal
    Fixed a bug where clicking on the textbox in the modal would refresh the page
    
    Criteria for the specific movie tile/box shows up on the corresponding modal clicked based on it's row and column

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // Selecting Elements
-const movieGuessModal = document.querySelector('#movieGuessModal')
-const movieGuessCloseBtn = document.querySelector('#movieGuessCloseBtn')
-const movieBoxes = document.querySelectorAll('.movieBox')
+const movieGuessModal = document.querySelector('#movieGuessModal');
+const movieGuessCloseBtn = document.querySelector('#movieGuessCloseBtn');
+const movieBoxes = document.querySelectorAll('.movieBox');
+const columnCriteria = [
+  document.getElementById('column1'),
+  document.getElementById('column2'),
+  document.getElementById('column3')
+];
 
-movieBoxes.forEach(box => {
+const rowCriteria = [
+  document.getElementById('row1'),
+  document.getElementById('row2'),
+  document.getElementById('row3')
+];
+
+movieBoxes.forEach((box, index) => {
   box.addEventListener('click', () => {
-    //Get the criteria details
-    // const movieCriteria = 
+    // Calculating box location
+    console.log(index);
+    const rowIndex = Math.floor(index / 3); 
+    const colIndex = (index % 3); 
+
+    // Getting the criteria details
+    const rowText = rowCriteria[rowIndex].textContent;
+    const columnText = columnCriteria[colIndex].textContent;
+
     // Show the modal
     movieGuessModal.style.display = 'block';
+    // Display the criteria details via temperal literal
+    document.getElementById('movieCriteria').textContent = `${rowText} X ${columnText}`;
   })
 })
 
