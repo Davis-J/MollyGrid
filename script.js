d0ef7bb5096cf7ebde6e56b8a70ffdacb1037586
Davis <davisjohnmutholil@gmail.com>
2025-08-10 23:31:01 +0100

    Added API Connection for Search Queries
    Added API connection to the movie database website
    When searching for movie guesses, it shows relevant guesses with each character input
    However guesses still cannot be submitted

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Global variables
 let activeBox = null; // this stores which box was clicked
 
+// API constants
+const apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjE1MTgwNDU1NTVmOTA4OTBiODQ2NTkyY2M4OGRmNyIsIm5iZiI6MTc1MDg4ODUwMS42NjksInN1YiI6IjY4NWM3MDM1MWMxZTQ2NDdhMDE1NTRhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gqLGCXdcaAQF-YIwj9Lfcll8ASr6Y6dmjpK-9tEG3EA";
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${apiKey}`
+  }
+};
 // Selecting Elements
 const movieGuessModal = document.querySelector('#movieGuessModal');
 const movieGuessCloseBtn = document.querySelector('#movieGuessCloseBtn');
@@ -8,6 +17,8 @@ const movieBoxes = document.querySelectorAll('.movieBox');
 const movieInput = document.querySelector('#movieInput')
 const movieList = document.querySelector('#movieList');
 const movieListItems = document.querySelectorAll('.movieListItem')
+
+// Constant string or integer values
 const columnCriteria = [
   document.getElementById('column1'),
   document.getElementById('column2'),
@@ -19,7 +30,9 @@ const rowCriteria = [
   document.getElementById('row2'),
   document.getElementById('row3')
 ];
+const movieSearchListLength = 5;
 
+//Event Listeners
 movieBoxes.forEach((box, index) => {
   box.addEventListener('click', () => {
     // Calculating box location
@@ -58,6 +71,7 @@ function filterFunction() {
   var input, filter, ul, li, i, txtValue;
   input = document.getElementById('movieInput');
   filter = input.value.toUpperCase();
+  getMovieList(filter);
   li = movieList.getElementsByTagName('li');
 
   // Loop through all list items, and hide those who don't match the search query
@@ -86,4 +100,42 @@ function resetMovieListItems() {
   for (i = 0; i < li.length; i++) {
     li[i].style.display = "";
   }
+}
+
+//API CALLS ETC
+
+//await means wait until asynchronous action is complete and then move on
+
+async function getMovieList(query) {
+  const getMovieListAPI = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+  try {
+    const response = await fetch (getMovieListAPI, options);
+
+    if (!response.ok) {
+      console.log ("This query couldn't be resolved");
+    }
+    console.log(response);
+    data = await response.json();
+    
+    console.log(data.results.length);
+    let movieSearchList = []
+    movieList.innerHTML = ""
+    for (i = 0; i<data.results.length; i++) {
+      movieList.innerHTML += `<li class="movieListItem">${data.results[i].title}</li>`
+      movieSearchList.push(data.results[i].title);
+      if (i == movieSearchListLength - 1) {
+        break;
+      }
+    }
+    console.log(movieSearchList);
+    
+  }
+  catch(error) {
+    console.error(error);
+  } 
+  // RANDOM CODE THOUGHTS
+  // take input from the search bar
+  // use the above api function to get a maximum of 15 movies with that title
+  // take the relevant information from what is returned
+  // Use the relevant information to display on the drop down list
 }
